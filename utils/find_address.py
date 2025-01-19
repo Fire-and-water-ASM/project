9a785878276008a3135bcd3024958e729a69aa13
Pedro Henrique Cardoso de Souza <pedrohcsousa8@hotmail.com>
2025-01-19 02:18:24 -0300

    Adding utils

---
+++ utils/find_address.py
@@ -0,0 +1,21 @@
+def calculate_address(vA, vB):
+    vA = vA * 4
+    vB = vB * 1024
+    c = vA + vB
+    return c
+
+def main():
+    print("Ctrl + C to Stop.\nEnter X and Y\n")
+    array = list()
+    try:
+        while True:
+            x = int(input())
+            y = int(input())
+            z = calculate_address(x, y)
+            array.append(z)
+    except KeyboardInterrupt:
+        print('\n\nResults:')
+        for i in array:
+            print(i)
+
+main()
